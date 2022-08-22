1635c5d56af21b029fffb932394a26e725d9c530
KrivovDE <55reigen55@mail.ru>
2022-08-23 00:11:26 +0300

    added model Author

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
-from schemas import Book
+from typing import List
 
+
+from fastapi import FastAPI, Query, Path, Body
+from schemas import Book, Author
 
 app = FastAPI()
 
@@ -26,5 +28,53 @@ def get_user_item(pk: int, item: str):
 
 
 @app.post('/book')
-def create_book(item: Book):
-    return item
+def create_book(item: Book, author: Author, quantity: int = Body(...,)):
+    return {'item': item, 'author': author, 'quantity': quantity}
+
+
+@app.get('/book')
+def get_book(q: List[str] = Query([], description='Search book', deprecated=True)):
+    return q
+
+
+@app.get('/book/{pk}')
+def get_single_book(pk: int = Path(..., gt=1, le=20),
+                    pages: int = Query(None, gt=10, le=50)):
+    return {'pk': pk,
+            'pages': pages
+            }
+
+
+@app.post('/author')
+def create_author(author: Author = Body(..., embed=True)):
+    return {'author': author}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
