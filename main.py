a253c960d55ae8fcc9505aec8dc03f20ad713fb5
KrivovDE <55reigen55@mail.ru>
2022-08-22 22:48:39 +0300

    added schemas

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from schemas import Book
+
 
 app = FastAPI()
 
@@ -16,3 +18,13 @@ async def say_hello(name: str):
 @app.get('/{pk}')
 def get_item(pk: int, q: str = None):
     return {'key': pk, 'q': q}
+
+
+@app.get('/user/{pk}/items/{item}')
+def get_user_item(pk: int, item: str):
+    return {'user': pk, 'item': item}
+
+
+@app.post('/book')
+def create_book(item: Book):
+    return item
